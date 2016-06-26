1955fd67ffb5c4cf0207de002efbe96b45b3cbb6
Archit Garg <architgarg1997@gmail.com>
2016-06-27 00:30:48 +0530

    Day13 Code added

---
+++ Day13.java
@@ -0,0 +1,47 @@
+ // @author Archit
+
+import java.util.*;
+abstract class Book
+{
+    String title;
+    String author;
+    Book(String t,String a){
+        title=t;
+        author=a;
+    }
+    abstract void display();
+
+
+}
+
+//Write MyBook Class
+
+class MyBook extends Book{
+    int price;
+    
+    public MyBook(String title, String author, int price){
+        super(title,author);
+        this.price = price;
+    }
+    
+    public void display(){
+        System.out.println("Title: " + this.title);
+        System.out.println("Author: " + this.author);
+        System.out.println("Price: " + this.price);     
+    }
+}
+
+public class Solution
+{
+   
+   public static void main(String []args)
+   {
+      Scanner sc=new Scanner(System.in);
+      String title=sc.nextLine();
+      String author=sc.nextLine();
+      int price=sc.nextInt();
+      Book new_novel=new MyBook(title,author,price);
+      new_novel.display();
+      
+   }
+}
